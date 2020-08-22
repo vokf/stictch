0903f6fa521b1c72307ece30f864d986eff37778
demo <2297802467@qq.com>
2020-08-22 21:11:04 +0800

    StApplication.java

--- src/main/java/com/stictch/StApplication.java
+++ src/main/java/com/stictch/StApplication.java
@@ -1,11 +1,18 @@
 package com.stictch;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author demo
  * @title StictchAppLication
  * @description
  * @date 2020/08/22/21:07
  */
+@SpringBootApplication
 
 public class StApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StApplication.class);
+    }
 }
