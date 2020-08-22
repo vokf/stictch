16bdc8726eebe742076a672a6b5ebfcfdb85bafc
demo <2297802467@qq.com>
2020-08-22 21:26:30 +0800

    User.java

--- src/main/java/com/stictch/entity/User.java
+++ src/main/java/com/stictch/entity/User.java
@@ -1,11 +1,30 @@
 package com.stictch.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * @author demo
  * @title User
  * @description
  * @date 2020/08/22/21:02
  */
-
+@Setter
+@Getter
+@ToString
 public class User {
+
+    private Integer userId;
+    /**
+     *
+     */
+    private String  userName;
+    private String passWord;
+    private String phone;
+    private Integer age;
+    private String email;
+    private String address;
+    private String role;
+
 }
