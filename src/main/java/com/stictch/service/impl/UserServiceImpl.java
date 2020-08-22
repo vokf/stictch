86b21a902665b912fa936b3d5eed605e4d488a69
demo <2297802467@qq.com>
2020-08-23 07:51:49 +0800

    @Service

--- src/main/java/com/stictch/service/impl/UserServiceImpl.java
+++ src/main/java/com/stictch/service/impl/UserServiceImpl.java
@@ -1,11 +1,31 @@
 package com.stictch.service.impl;
 
+import com.stictch.dao.UserDao;
+import com.stictch.entity.User;
+import com.stictch.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * @author demo
  * @title UserServiceImpl
  * @description
  * @date 2020/08/22/21:03
  */
+@Service
+public class UserServiceImpl implements UserService {
 
-public class UserServiceImpl {
+    private UserDao dao;
+
+    @Autowired
+    public void setDao(UserDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return dao.findAll();
+    }
 }
