a879235f2fecc877cf61020591f462177678787f
demo <2297802467@qq.com>
2020-08-23 07:51:37 +0800

    @Mapper

--- src/main/java/com/stictch/dao/UserDao.java
+++ src/main/java/com/stictch/dao/UserDao.java
@@ -1,11 +1,26 @@
 package com.stictch.dao;
 
+import com.stictch.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 /**
  * @author demo
  * @title UserDao
  * @description
  * @date 2020/08/22/21:03
  */
-
+@Mapper
+@Repository
 public interface UserDao {
+
+    /**
+     * 查询所有
+     * @return all
+     */
+    List<User> findAll();
+
+
 }
