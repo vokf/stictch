35d8d90577e184906f95456596877a4838a896e7
demo <2297802467@qq.com>
2020-08-22 21:05:55 +0800

    init

--- src/main/java/com/stictch/controller/UserController.java
+++ src/main/java/com/stictch/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.stictch.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author demo
  * @title UserController
  * @description
  * @date 2020/08/22/20:42
  */
-
+@RestController
+@RequestMapping("user")
 public class UserController {
+
 }
