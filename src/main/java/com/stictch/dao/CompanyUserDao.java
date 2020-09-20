84dc9d5f71e392007c0ee02d26ef78cff37b9c92
demo <2297802467@qq.com>
2020-09-20 15:38:16 +0800

    CompanyUserDao.xml

---
+++ src/main/java/com/stictch/dao/CompanyUserDao.java
@@ -0,0 +1,14 @@
+package com.stictch.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author demo
+ * @title CompanyUserDao
+ * @description
+ * @date 2020/09/20/15:32
+ */
+@Mapper
+
+public interface CompanyUserDao {
+}
