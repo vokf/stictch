b6edc76fe9a9394c31b5e16ccbb4272bf8c00156
demo <2297802467@qq.com>
2020-09-24 08:47:14 +0800

    Role.java

---
+++ src/main/java/com/stictch/entity/Role.java
@@ -0,0 +1,37 @@
+package com.stictch.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author demo
+ * @title CompanyUser
+ * @description
+ * @date 2020/09/20/15:27
+ */
+@Setter
+@Getter
+@ToString
+public class Role implements Serializable {
+    /**
+     * roleId
+     */
+    private Integer roleId;
+    /**
+     * 角色名称
+     */
+    private String roleName;
+    /**
+     * userId
+     */
+    private Integer userId;
+    /**
+     * powerId
+     */
+    private Integer powerId;
+    private List<Power> powerList;
+}
