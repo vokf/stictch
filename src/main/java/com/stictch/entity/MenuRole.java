f0119701ca7984ce810bfa8e142716a750cd70f8
demo <2297802467@qq.com>
2020-09-26 22:10:47 +0800

    MenuRole.java

---
+++ src/main/java/com/stictch/entity/MenuRole.java
@@ -0,0 +1,23 @@
+package com.stictch.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+/**
+ * @author demo
+ * @title MenuRole
+ * @description
+ * @date 2020/09/26/21:45
+ */
+@ToString
+@Setter
+@Getter
+public class MenuRole {
+    private Integer id;
+    private Integer menuId;
+    private Integer roleId;
+    private List<Menu> menuList;
+}
