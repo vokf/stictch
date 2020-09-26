012ca412b8a86d80b957a36352f4a8890ac3ea2b
demo <2297802467@qq.com>
2020-09-26 22:12:14 +0800

    UserServiceImpl.java

--- src/main/java/com/stictch/service/impl/UserServiceImpl.java
+++ src/main/java/com/stictch/service/impl/UserServiceImpl.java
@@ -3,10 +3,11 @@ package com.stictch.service.impl;
 import com.stictch.dao.UserDao;
 import com.stictch.entity.User;
 import com.stictch.service.UserService;
-
-import lombok.extern.log4j.Log4j;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,9 +19,10 @@ import org.springframework.transaction.annotation.Transactional;
  * @date 2020/08/22/21:03
  */
 @Service
-@Log4j
+@Slf4j
 @Transactional(rollbackFor = Exception.class)
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
+
 
     private UserDao dao;
 
@@ -40,7 +42,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User userLogin(User user) {
-        if (user.getUserName() == null || user.getPassWord() == null) {
+        if (user.getUsername() == null || user.getPassword() == null) {
             log.error("用户名或者密码为空");
             return null;
         } else {
@@ -52,13 +54,20 @@ public class UserServiceImpl implements UserService {
     @Override
     public int userRegister(User user) {
 
-        if (user.getUserName() == null || user.getPassWord() == null || user.getPhone() == null || user.getEmail() == null) {
+        if (user.getUsername() == null ||user.getPassword() == null || user.getPhone() == null || user.getEmail() == null) {
 
             log.error("用户名或者密码或者电话号码，或者邮箱为空");
             return 0;
         } else {
             return dao.userRegister(user);
         }
+
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+
+        return null;
 
     }
 }
