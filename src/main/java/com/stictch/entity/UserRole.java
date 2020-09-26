cfab0213b56aba661371aee7c0518cc6745ff981
demo <2297802467@qq.com>
2020-09-26 22:12:06 +0800

    UserRole.java

---
+++ src/main/java/com/stictch/entity/UserRole.java
@@ -0,0 +1,23 @@
+package com.stictch.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+/**
+ * @author demo
+ * @title UserRole
+ * @description
+ * @date 2020/09/26/21:55
+ */
+@Getter
+@Setter
+@ToString
+public class UserRole {
+    private Integer id;
+    private Integer userId;
+    private Integer roleId;
+    private List<User> userList;
+}
