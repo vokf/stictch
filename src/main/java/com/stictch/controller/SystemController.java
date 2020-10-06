454d33251cc017c3a9e29195e8a59c4f09677c9d
demo <2297802467@qq.com>
2020-10-06 20:21:28 +0800

    SystemController.java

---
+++ src/main/java/com/stictch/controller/SystemController.java
@@ -0,0 +1,30 @@
+package com.stictch.controller;
+
+import com.stictch.entity.Menu;
+import com.stictch.entity.User;
+import com.stictch.service.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author demo
+ * @title SystemConfigController
+ * @description
+ * @date 2020/09/27/22:11
+ */
+@RestController
+@RequestMapping("/system/config")
+public class SystemController {
+    @Autowired
+    private MenuService menuService;
+
+//    @GetMapping("/menu")
+//    public List<Menu> getMenuByUserId() {
+//        return menuService.getMenuByUserId(((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUserId());
+//    }
+}
