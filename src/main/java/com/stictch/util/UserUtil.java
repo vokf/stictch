ec10a5ded41bb0939136104d7ae74a95b745ffa8
demo <2297802467@qq.com>
2020-10-06 20:20:19 +0800

    UserUtil.java

---
+++ src/main/java/com/stictch/util/UserUtil.java
@@ -0,0 +1,17 @@
+package com.stictch.util;
+
+import com.stictch.entity.User;
+//import org.springframework.security.core.context.SecurityContextHolder;
+
+/**
+ * @author demo
+ * @title UserUtil
+ * @description
+ * @date 2020/09/27/20:20
+ */
+
+public class UserUtil {
+//    public static User getCurrentUser(){
+//        return ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+//    }
+}
