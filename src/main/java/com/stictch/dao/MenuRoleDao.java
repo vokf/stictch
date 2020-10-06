6617a1252ac1aa5224d431307d119a840cf25529
demo <2297802467@qq.com>
2020-10-06 20:22:00 +0800

    MenuRoleDao.java

---
+++ src/main/java/com/stictch/dao/MenuRoleDao.java
@@ -0,0 +1,16 @@
+package com.stictch.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author demo
+ * @title MenuRoleDao
+ * @description
+ * @date 2020/09/27/19:43
+ */
+
+@Mapper
+@Repository
+public interface MenuRoleDao {
+}
