7f7682f849e83570110ce95823329c7b1d4cefbc
demo <2297802467@qq.com>
2020-10-06 20:19:25 +0800

    UserDao.java

--- src/main/java/com/stictch/dao/UserDao.java
+++ src/main/java/com/stictch/dao/UserDao.java
@@ -1,5 +1,6 @@
 package com.stictch.dao;
 
+import com.stictch.entity.Role;
 import com.stictch.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -19,12 +20,14 @@ public interface UserDao {
 
     /**
      * 查询所有
+     *
      * @return all
      */
     User findByUserName(@Param("userName") String userName);
 
     /**
      * 登录
+     *
      * @param user user
      * @return User
      */
@@ -32,10 +35,36 @@ public interface UserDao {
 
     /**
      * 注册
+     *
      * @param user user
      * @return int
      */
     int userRegister(User user);
 
+    /**
+     * @param id
+     * @return
+     */
+    List<Role> getUserRolesById(Integer id);
+
+    /**
+     * @param userName
+     * @return
+     */
+    User loadUserByUserName(String userName);
+
+    /**
+     * @param userId
+     * @param keywords
+     * @return
+     */
+    List<User> getAllUsers(@Param("userId") Integer userId, @Param("keywords") String keywords);
+
+    /**
+     * 通过用户名查询该用户的所有信息
+     * @param userId
+     * @return
+     */
+    List<User> findUserById(@Param("userId" ) Integer userId);
 
 }
