e4fb3d38c32bc7d455a569921c9a783ed288d0a7
demo <2297802467@qq.com>
2020-10-06 20:20:06 +0800

    UserServiceImpl.java

--- src/main/java/com/stictch/service/impl/UserServiceImpl.java
+++ src/main/java/com/stictch/service/impl/UserServiceImpl.java
@@ -1,15 +1,21 @@
 package com.stictch.service.impl;
 
 import com.stictch.dao.UserDao;
+import com.stictch.dao.UserRoleDao;
+import com.stictch.entity.Role;
 import com.stictch.entity.User;
 import com.stictch.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 /**
@@ -23,8 +29,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(rollbackFor = Exception.class)
 public class UserServiceImpl implements UserService, UserDetailsService {
 
+    @Autowired
+    BCryptPasswordEncoder encoder;
+
+    private UserRoleDao role;
 
     private UserDao dao;
+
+    @Autowired
+    public void setRole(UserRoleDao role) {
+        this.role = role;
+    }
+
 
     @Autowired
     public void setDao(UserDao dao) {
@@ -41,12 +57,18 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     }
 
     @Override
-    public User userLogin(User user) {
-        if (user.getUsername() == null || user.getPassword() == null) {
+    public User userLogin(String userName, String password) {
+
+        if (userName == null || password == null) {
             log.error("用户名或者密码为空");
             return null;
         } else {
-            return dao.userLogin(user);
+            User user = dao.findByUserName(userName);
+            if (user != null && encoder.matches(password, user.getPassword())) {
+                return user;
+            } else {
+                return null;
+            }
         }
 
     }
@@ -54,20 +76,60 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     @Override
     public int userRegister(User user) {
 
-        if (user.getUsername() == null ||user.getPassword() == null || user.getPhone() == null || user.getEmail() == null) {
+        if (user.getUsername() == null || user.getPassword() == null || user.getPhone() == null || user.getEmail() == null) {
 
             log.error("用户名或者密码或者电话号码，或者邮箱为空");
             return 0;
         } else {
+            String encode = encoder.encode(user.getPassword());
+            user.setPassWord(encode);
             return dao.userRegister(user);
+
         }
 
     }
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+    public List<Role> getUserRolesById(Integer id) {
+        return dao.getUserRolesById(id);
+    }
+
+    @Override
+    public User loadUserByUserName(String userName) {
+        return dao.loadUserByUserName(userName);
+    }
+
+    @Override
+    public List<User> getAllUsers(Integer userId, String keywords) {
 
         return null;
+    }
 
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        log.info("表单登录用户名:" + s);
+        User user = dao.loadUserByUserName(s);
+        if (user == null) {
+            throw new UsernameNotFoundException("用户名不存在!");
+        }
+
+        user.setRoles(dao.getUserRolesById(user.getUserId()));
+        return user;
+
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    public List<User> findUserById(Integer userId) {
+        if (userId == 0) {
+            return null;
+        } else {
+           return dao.findUserById(userId);
+        }
     }
 }
