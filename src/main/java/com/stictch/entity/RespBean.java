79ac8ff8dbc5c9f05da04db7885f5acad176e043
demo <2297802467@qq.com>
2020-10-06 20:18:13 +0800

    RespBean.java

---
+++ src/main/java/com/stictch/entity/RespBean.java
@@ -0,0 +1,52 @@
+package com.stictch.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+
+/**
+ * @author demo
+ * @title Result
+ * @description
+ * @date 2020/09/19/9:19
+ */
+@Getter
+@Setter
+public class RespBean implements Serializable {
+    private Integer status;
+    private String msg;
+    private Object obj;
+
+    public static RespBean build() {
+        return new RespBean();
+    }
+
+    public static RespBean ok(String msg) {
+        return new RespBean(200, msg, null);
+    }
+
+    public static RespBean ok(String msg, Object obj) {
+        return new RespBean(200, msg, obj);
+    }
+
+    public static RespBean error(String msg) {
+        return new RespBean(500, msg, null);
+    }
+
+    public static RespBean error(String msg, Object obj) {
+        return new RespBean(500, msg, obj);
+    }
+
+    private RespBean() {
+    }
+
+    private RespBean(Integer status, String msg, Object obj) {
+        this.status = status;
+        this.msg = msg;
+        this.obj = obj;
+    }
+
+
+}
