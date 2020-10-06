2f6fd5d1420d1a9b090ec5134791925bcf32e32f
demo <2297802467@qq.com>
2020-10-06 20:18:44 +0800

    SecurityConfig.java

---
+++ src/main/java/com/stictch/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.stictch.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author demo
+ * @title SecurityConfig
+ * @description
+ * @date 2020/09/28/12:10
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll().
+                anyRequest().authenticated().and().csrf().disable();
+
+    }
+}
