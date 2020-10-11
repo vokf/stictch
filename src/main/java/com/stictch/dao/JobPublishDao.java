7974c2b111a5e5b266b8ead2ecb86ccf979a631c
demo <2297802467@qq.com>
2020-10-11 16:53:07 +0800

    dao

---
+++ src/main/java/com/stictch/dao/JobPublishDao.java
@@ -0,0 +1,15 @@
+package com.stictch.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author demo
+ * @title JobPublishDao
+ * @description
+ * @date 2020/10/10/18:44
+ */
+@Repository
+@Mapper
+public interface JobPublishDao {
+}
