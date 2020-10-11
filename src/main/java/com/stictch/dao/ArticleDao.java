fcde0d91a7b6984c00c7887933d810309153f104
demo <2297802467@qq.com>
2020-10-11 16:52:52 +0800

    dao

---
+++ src/main/java/com/stictch/dao/ArticleDao.java
@@ -0,0 +1,16 @@
+package com.stictch.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author demo
+ * @title ArticleDao
+ * @description
+ * @date 2020/10/10/11:40
+ */
+@Mapper
+@Repository
+public interface ArticleDao {
+
+}
