dd8f925942ccef163168c3254c5e4fc370111a2a
demo <2297802467@qq.com>
2020-10-16 14:19:30 +0800

    entity

--- src/main/java/com/stictch/entity/Article.java
+++ src/main/java/com/stictch/entity/Article.java
@@ -1,11 +1,13 @@
 package com.stictch.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.apache.ibatis.type.JdbcType;
 
 import java.io.Serializable;
 
@@ -22,11 +24,17 @@ import java.io.Serializable;
 public class Article implements Serializable {
     @TableId(value = "article_id", type = IdType.AUTO)
     private Integer articleId;
+    @TableField(value = "article_big_title", jdbcType = JdbcType.VARCHAR)
     private String bigTitle;
+    @TableField(value = "article_small_title", jdbcType = JdbcType.VARCHAR)
     private String smallTitle;
+    @TableField(value = "article_content", jdbcType = JdbcType.VARCHAR)
     private String content;
+    @TableField(value = "article_image", jdbcType = JdbcType.VARCHAR)
     private String image;
+    @TableField(value = "article_video", jdbcType = JdbcType.VARCHAR)
     private String video;
+    @TableField(value = "article_upload_user_id", jdbcType = JdbcType.INTEGER)
     private Integer uploadUserId;
 
 
