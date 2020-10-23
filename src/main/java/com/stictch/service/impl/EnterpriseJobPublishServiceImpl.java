1409fbba435c49c738c14c52f19578ecdc444654
demo <2297802467@qq.com>
2020-10-23 11:31:40 +0800

    service

---
+++ src/main/java/com/stictch/service/impl/EnterpriseJobPublishServiceImpl.java
@@ -0,0 +1,38 @@
+package com.stictch.service.impl;
+
+
+
+import com.stictch.entity.EnterpriseJobPublish;
+import com.stictch.service.EnterpriseJobPublishService;
+
+
+import java.util.List;
+
+/**
+ * @author demo
+ * @title EnterpriseJobPublishServiceImpl
+ * @description
+ * @date 2020/10/23/9:51
+ */
+
+public class EnterpriseJobPublishServiceImpl implements EnterpriseJobPublishService {
+    @Override
+    public EnterpriseJobPublish queryById(Long publishId) {
+        return null;
+    }
+
+    @Override
+    public EnterpriseJobPublish insert(EnterpriseJobPublish enterpriseJobPublish) {
+        return null;
+    }
+
+    @Override
+    public EnterpriseJobPublish update(EnterpriseJobPublish enterpriseJobPublish) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteById(Long publishId) {
+        return false;
+    }
+}
