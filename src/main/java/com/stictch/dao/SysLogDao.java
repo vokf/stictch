a5f24434a41fc1856060e79cc7a43abc06d1a571
demo <2297802467@qq.com>
2020-10-23 11:33:19 +0800

    dao

---
+++ src/main/java/com/stictch/dao/SysLogDao.java
@@ -0,0 +1,50 @@
+package com.stictch.dao;
+
+import com.stictch.entity.SysLog;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @author demo
+ * @title SysLogDao
+ * @description
+ * @date 2020/10/23/10:26
+ */
+@Mapper
+@Repository
+public interface SysLogDao {
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param sysLogId 主键
+     * @return 实例对象
+     */
+    SysLog queryById(Long sysLogId);
+
+    /**
+     * 通过实体作为筛选条件查询
+     *
+     * @param sysLog 实例对象
+     * @return 对象列表
+     */
+    List<SysLog> queryAll(SysLog sysLog);
+
+    /**
+     * 新增数据
+     *
+     * @param sysLog 实例对象
+     * @return 影响行数
+     */
+    int insert(SysLog sysLog);
+
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param sysLogId 主键
+     * @return 影响行数
+     */
+    int deleteById(Long sysLogId);
+}
