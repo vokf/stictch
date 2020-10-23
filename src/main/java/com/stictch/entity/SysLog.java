c86a80df0292028f9f8596df54c43e9102a9c4e3
demo <2297802467@qq.com>
2020-10-23 11:30:48 +0800

    serviceimpl

---
+++ src/main/java/com/stictch/entity/SysLog.java
@@ -0,0 +1,37 @@
+package com.stictch.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author demo
+ * @title SysLog
+ * @description
+ * @date 2020/10/23/10:22
+ */
+@Getter
+@Setter
+public class SysLog implements Serializable {
+    private static final long serialVersionUID = -70337611397921482L;
+    /**
+     * id
+     */
+    private Long sysLogId;
+    /**
+     * 操作用户
+     */
+    private String userName;
+    /**
+     * 操作时间
+     */
+    private Date sysLogOperateTime;
+    /**
+     * 干了什么操作
+     */
+    private String sysUserDoWhatOperate;
+    private Integer userId;
+
+}
---
+++ src/main/java/com/stictch/service/impl/AdminCheckingServiceImpl.java
@@ -0,0 +1,37 @@
+package com.stictch.service.impl;
+
+import com.stictch.entity.AdminChecking;
+import com.stictch.service.AdminCheckingService;
+
+import java.util.List;
+
+/**
+ * 管理员检查(AdminChecking)表服务实现类
+ *
+ * @author demo
+ * @title AdminCheckingServiceImpl
+ * @description
+ * @date 2020/10/23/9:48
+ */
+
+public class AdminCheckingServiceImpl implements AdminCheckingService {
+    @Override
+    public AdminChecking queryById(Long adminCheckId) {
+        return null;
+    }
+
+    @Override
+    public AdminChecking insert(AdminChecking adminChecking) {
+        return null;
+    }
+
+    @Override
+    public AdminChecking update(AdminChecking adminChecking) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteById(Long adminCheckId) {
+        return false;
+    }
+}
