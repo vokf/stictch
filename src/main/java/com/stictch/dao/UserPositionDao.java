f24320ad891d7e632a8f7df7d04e7d4c5c33cbbe
demo <2297802467@qq.com>
2020-10-23 11:35:29 +0800

    dao

---
+++ src/main/java/com/stictch/dao/UserPositionDao.java
@@ -0,0 +1,71 @@
+package com.stictch.dao;
+
+
+import com.stictch.entity.UserPosition;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * 职位(UserPosition)表数据库访问层
+ * @author demo
+ * @title UserPositionDao
+ * @description
+ * @date 2020/10/23/10:52
+ */
+@Mapper
+@Repository
+public interface UserPositionDao {
+
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param positionId 主键
+     * @return 实例对象
+     */
+    UserPosition queryById(Long positionId);
+
+    /**
+     * 查询指定行数据
+     *
+     * @param offset 查询起始位置
+     * @param limit  查询条数
+     * @return 对象列表
+     */
+    List<UserPosition> queryAllByLimit(@Param("offset") int offset, @Param("limit") int limit);
+
+    /**
+     * 通过实体作为筛选条件查询
+     *
+     * @param userPosition 实例对象
+     * @return 对象列表
+     */
+    List<UserPosition> queryAll(UserPosition userPosition);
+
+    /**
+     * 新增数据
+     *
+     * @param userPosition 实例对象
+     * @return 影响行数
+     */
+    int insert(UserPosition userPosition);
+
+    /**
+     * 修改数据
+     *
+     * @param userPosition 实例对象
+     * @return 影响行数
+     */
+    int update(UserPosition userPosition);
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param positionId 主键
+     * @return 影响行数
+     */
+    int deleteById(Long positionId);
+}
