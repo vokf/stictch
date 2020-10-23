7c804525fbcbd2b7f49c48f3dc09e68317369ec2
demo <2297802467@qq.com>
2020-10-23 11:32:37 +0800

    serviceImpl

---
+++ src/main/java/com/stictch/service/impl/PositionServiceImpl.java
@@ -0,0 +1,40 @@
+package com.stictch.service.impl;
+
+import com.stictch.entity.UserPosition;
+import com.stictch.service.PositionService;
+
+import java.util.List;
+
+/**
+ * @author demo
+ * @title PositionServiceImpl
+ * @description
+ * @date 2020/10/23/11:00
+ */
+
+public class PositionServiceImpl implements PositionService {
+    @Override
+    public UserPosition queryById(Long positionId) {
+        return null;
+    }
+
+    @Override
+    public List<UserPosition> queryAllByLimit(int offset, int limit) {
+        return null;
+    }
+
+    @Override
+    public UserPosition insert(UserPosition userPosition) {
+        return null;
+    }
+
+    @Override
+    public UserPosition update(UserPosition userPosition) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteById(Long positionId) {
+        return false;
+    }
+}
