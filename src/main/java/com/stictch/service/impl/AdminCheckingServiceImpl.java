d590735d0d226a0a2aa3e9d5d71eeba45f283a6f
demo <2297802467@qq.com>
2020-10-25 22:41:47 +0800

    serviceImpl

--- src/main/java/com/stictch/service/impl/AdminCheckingServiceImpl.java
+++ src/main/java/com/stictch/service/impl/AdminCheckingServiceImpl.java
@@ -2,6 +2,8 @@ package com.stictch.service.impl;
 
 import com.stictch.entity.AdminChecking;
 import com.stictch.service.AdminCheckingService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,7 +15,8 @@ import java.util.List;
  * @description
  * @date 2020/10/23/9:48
  */
-
+@Service
+@Transactional(rollbackFor = Exception.class)
 public class AdminCheckingServiceImpl implements AdminCheckingService {
     @Override
     public AdminChecking queryById(Long adminCheckId) {
