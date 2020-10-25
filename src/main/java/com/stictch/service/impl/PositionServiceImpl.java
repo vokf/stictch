c931386b12b8a80707546030a28620756667b7d5
demo <2297802467@qq.com>
2020-10-25 22:45:14 +0800

    service

--- src/main/java/com/stictch/service/impl/PositionServiceImpl.java
+++ src/main/java/com/stictch/service/impl/PositionServiceImpl.java
@@ -1,7 +1,12 @@
 package com.stictch.service.impl;
 
+import com.stictch.dao.UserPositionDao;
 import com.stictch.entity.UserPosition;
 import com.stictch.service.PositionService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,11 +16,26 @@ import java.util.List;
  * @description
  * @date 2020/10/23/11:00
  */
-
+@Slf4j
+@Service
+@Transactional(rollbackFor = Exception.class)
 public class PositionServiceImpl implements PositionService {
+
+    private UserPositionDao positionDao;
+
+    @Autowired
+    public void setPositionDao(UserPositionDao positionDao) {
+        this.positionDao = positionDao;
+    }
+
     @Override
     public UserPosition queryById(Long positionId) {
-        return null;
+        if (positionId != null) {
+            return positionDao.queryById(positionId);
+        } else {
+            log.debug("postionId是空的");
+            return null;
+        }
     }
 
     @Override
@@ -36,5 +56,10 @@ public class PositionServiceImpl implements PositionService {
     @Override
     public boolean deleteById(Long positionId) {
         return false;
+    }
+
+    @Override
+    public List<UserPosition> queryAll() {
+        return positionDao.queryAll();
     }
 }
