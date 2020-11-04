8db4788a664dfed6d42a56acdfd0eedfac323dde
demo <2297802467@qq.com>
2020-11-04 08:58:22 +0800

    impl

--- src/main/java/com/stictch/service/impl/AdminCheckingServiceImpl.java
+++ src/main/java/com/stictch/service/impl/AdminCheckingServiceImpl.java
@@ -1,11 +1,11 @@
 package com.stictch.service.impl;
 
+import com.stictch.dao.AdminCheckingDao;
 import com.stictch.entity.AdminChecking;
 import com.stictch.service.AdminCheckingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 /**
  * 管理员检查(AdminChecking)表服务实现类
@@ -18,23 +18,50 @@ import java.util.List;
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class AdminCheckingServiceImpl implements AdminCheckingService {
+    AdminCheckingDao dao;
+
+    @Autowired
+    public void setDao(AdminCheckingDao dao) {
+        this.dao = dao;
+    }
+
     @Override
     public AdminChecking queryById(Long adminCheckId) {
-        return null;
+        if (adminCheckId != null) {
+            return dao.queryById(adminCheckId);
+        } else {
+            return null;
+        }
+
     }
 
     @Override
-    public AdminChecking insert(AdminChecking adminChecking) {
-        return null;
+    public Integer insert(AdminChecking adminChecking) {
+        if (adminChecking.getAdminCheckOperate() != null) {
+            return dao.insert(adminChecking);
+        } else {
+            return null;
+        }
+
     }
 
     @Override
-    public AdminChecking update(AdminChecking adminChecking) {
-        return null;
+    public Integer update(AdminChecking adminChecking) {
+        if (adminChecking.getAdminCheckOperate() != null){
+            return dao.update(adminChecking);
+        }else {
+            return null;
+        }
+
     }
 
     @Override
-    public boolean deleteById(Long adminCheckId) {
-        return false;
+    public Integer deleteById(Long adminCheckId) {
+        if (adminCheckId != null){
+            return dao.deleteById(adminCheckId);
+        }
+        else {
+            return 0;
+        }
     }
 }
